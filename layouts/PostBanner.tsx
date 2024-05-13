1ebd995d882773804cf578e52d9e37730c5e2e59
augustynglowacki <augustyn1331@gmail.com>
2024-05-13 14:58:57 +0200

    Post Layouts padding-x fixes

--- layouts/PostBanner.tsx
+++ layouts/PostBanner.tsx
@@ -6,7 +6,6 @@ import type { Blog } from 'contentlayer/generated'
 import Comments from '@/components/Comments'
 import Link from '@/components/Link'
 import PageTitle from '@/components/PageTitle'
-import SectionContainer from '@/components/SectionContainer'
 import siteMetadata from '@/data/siteMetadata'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
 
@@ -23,11 +22,11 @@ export default function PostMinimal({ content, next, prev, children }: LayoutPro
     images && images.length > 0 ? images[0] : 'https://picsum.photos/seed/picsum/800/400'
 
   return (
-    <SectionContainer>
+    <section>
       <ScrollTopAndComment />
       <article>
         <div>
-          <div className="pb-10 space-y-1 text-center border-decoration-700">
+          <div className="px-6 pb-10 space-y-1 text-center border-decoration-700">
             <div className="w-full">
               <Bleed>
                 <div className="relative aspect-[2/1] w-full">
@@ -73,6 +72,6 @@ export default function PostMinimal({ content, next, prev, children }: LayoutPro
           </footer>
         </div>
       </article>
-    </SectionContainer>
+    </section>
   )
 }
